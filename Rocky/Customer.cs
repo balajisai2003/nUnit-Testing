84f11930d19b4de26ab63ae88169c0ea986923e5
BalajiSai2003 <balajisai2003@outlook.com>
2025-02-19 15:34:53 +0530

    Add Customer class and unit tests for greeting method
    Introduces a new `Customer` class with a method `GreetAndCombineNames` that combines first and last names into a greeting string. Adds NUnit unit tests in `CustomerN-UnitTests.cs` to verify the method's output and structure.

---
+++ Rocky/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky
+{
+    public class Customer
+    {
+        public string GreetAndCombineNames(string firstName, string lastName)
+        {
+            return $"Hi {firstName} {lastName}";
+        }
+    }
+}
---
+++ RockyN-UnitTests/CustomerN-UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocky
+{
+    [TestFixture]
+    public class CustomerN_UnitTests
+    {
+        [Test]
+        [TestCase("Rocky", "Balboa", ExpectedResult = "Hi Rocky Balboa")]
+        [TestCase("Tom", "Jerry", ExpectedResult = "Hi Tom Jerry")]
+        [TestCase("Tom", "", ExpectedResult = "Hi Tom ")]
+        public string GreetAndCombineNames_WhenCalled_ReturnsGreetedFullName(string firstName, string lastName)
+        {
+            // Arrange
+            var customer = new Customer();
+            // Act
+            string result = customer.GreetAndCombineNames(firstName, lastName);
+            // Assert
+            return result;
+        }
+
+
+        [Test]
+        [TestCase("Rocky", "Balboa")]
+        [TestCase("Tom", "Jerry")]
+        public void GreetAndCombineNames_WhenCalled_ReturnsFullNameWithNecessaryThings(string firstName, string lastName)
+        {
+            // Arrange
+            var customer = new Customer();
+            // Act
+            string result = customer.GreetAndCombineNames(firstName, lastName);
+            // Assert
+            Assert.That(result.Split().Length, Is.EqualTo(3));
+            Assert.That(result, Does.Contain("Hi").IgnoreCase);
+            Assert.That(result, Does.StartWith("Hi"));
+            Assert.That(result, Does.EndWith(lastName));
+
+        }
+    }
+
+
+}
